37c11d87943206e997dccdcba7774a212c6c0af9
권영란IT1817 <it.youngran@gmail.com>
2025-07-09 09:17:05 +0900

    Part3.1.2 p.37

---
+++ src/main/java/com/example/studybatch/config/JobRunner.java
@@ -0,0 +1,33 @@
+package com.example.studybatch.config;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JobRunner implements ApplicationRunner {
+
+    private final JobLauncher jobLauncher;
+
+    private final Job job;
+
+    public JobRunner(JobLauncher jobLauncher, Job job) {
+        this.jobLauncher = jobLauncher;
+        this.job = job;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("name", "user3")
+                .toJobParameters();
+
+        jobLauncher.run(job, jobParameters);
+
+    }
+}
